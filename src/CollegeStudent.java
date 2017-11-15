f8e645dfd03c4f9f398aad4f5b146da5325896c3
swilliams19 <32960946+swilliams19@users.noreply.github.com>
2017-11-15 10:31:38 -0600

    S.Williams

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,15 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Stephanie Williams
+  	Date: 11/15/2017
 */
 
 public class CollegeStudent {
 
 	public static void main(String[] args) {
-
+		int year;
+		int courses;
+		double GPA;
 	}
 
 }
--- src/ComputePay.java
+++ src/ComputePay.java
@@ -1,18 +1,21 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Stephanie Williams	
+  	Date: 11/15/2017
 */
 
 public class ComputePay {
 	public static void main(String[] args) {
+		int totalhours = 4 + 5 + 8 + 4;
+		double salary = 8.75;
+		double totalpay = totalhours * salary;
+		
 		System.out.println("My total hours worked:");
-		System.out.println(4 + 5 + 8 + 4);
-		
+		System.out.println(totalhours);
 		System.out.println("My hourly salary:");
-		System.out.println("$8.75");
-		
+		System.out.println(salary);
 		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
-	}
+		System.out.println(totalpay);
 }
+	}
+	
